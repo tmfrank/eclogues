8094684c7f734463b9e75e0e0a2270013c1be631
Shannon Pace <futufeld@gmail.com>
2015-08-13 17:38:57 +1000

    ENHANCE: Capture more information when tests fail

--- test/StateSpec.hs
+++ test/StateSpec.hs
@@ -15,21 +15,38 @@ import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Except (ExceptT, runExceptT)
 import Data.Either (isRight, isLeft)
 import Data.UUID (nil)
-import Control.Lens ((^.), (.~), at)
+import Control.Lens (view, at, (^.), (.~))
 import Data.Maybe (isNothing)
 
 type Scheduler = ExceptT JobError ES.EState ()
-type Result = (Either JobError (), AppState)
+
+data TestError = EncounteredError JobError
+               | JobNotFound Name
+               | RevDepNotFound Name
+               | ExpectedError JobError
+               | UnexpectedError TestError
+                 deriving (Show)
+
+type EitherError a = Either TestError a
+
+maybeToEither :: a -> Maybe b -> Either a b
+maybeToEither def = maybe (Left def) Right
 
 defAppState :: AppState
 defAppState = def
 
-scheduler' :: Scheduler -> Result
+scheduler' :: Scheduler -> EitherError AppState
 scheduler' = scheduler defAppState
 
-scheduler :: AppState -> Scheduler -> Result
-scheduler aState m = (x, y ^. appState)
-    where (x, y) = ES.runEState aState . runExceptT $ m
+scheduler :: AppState -> Scheduler -> EitherError AppState
+scheduler aState = packageResult . fmap (^. appState) . ES.runEState aState . runExceptT
+
+packageResult :: (Either JobError (), AppState) -> EitherError AppState
+packageResult (Left jError, _) = Left (EncounteredError jError)
+packageResult (_, effect) = Right (effect ^. appState)
+
+shouldHave :: EitherError AppState -> (EitherError AppState -> EitherError Bool) -> Expectation
+shouldHave result f = result `shouldSatisfy` either (const False) id . f
 
 createJob' :: JobSpec -> Scheduler
 createJob' = createJob nil
@@ -41,56 +58,39 @@ isolatedJob x = JobSpec x "/bin/echo" res [] False []
 dependentJob :: Name -> [Name] -> JobSpec
 dependentJob job deps = dependsOn .~ deps $ isolatedJob job
 
-shouldHave :: Result -> (Result -> Either String Bool) -> Expectation
-shouldHave result f = result `shouldSatisfy` (either (const False) (id)) . f
+getJob :: Name -> AppState -> Maybe JobStatus
+getJob jName aState = aState ^. jobs ^. at jName
 
-getJob :: Name -> Result -> Either String JobStatus
-getJob jName result = maybe (Left "No job found.") Right job
-    where job = (snd result) ^. appState ^. jobs ^. at jName
+noJob :: Name -> EitherError AppState -> EitherError Bool
+noJob jName = fmap (isNothing . getJob jName)
 
-noJob :: Name -> Result -> Either String Bool
-noJob jName = validator
-    where validator result
-            | (isLeft . fst) result = Left "Process returned error."
-            | otherwise = Right $ isLeft (getJob jName result)
+jobInState :: Name -> JobState -> EitherError AppState -> EitherError Bool
+jobInState jName jState result = return . inState =<< eitherGetJob jName =<< result
+    where eitherGetJob jName = maybeToEither (JobNotFound jName) . getJob jName
+          inState = (== jState) . view jobState
 
-getRevDep :: Name -> Result -> Either String [Name]
-getRevDep jName result = maybe (Left "No reverse dependency found.") Right revDep
-    where revDep = (snd result) ^. appState ^. revDeps ^. at jName
+getRevDep :: Name -> AppState -> Maybe [Name]
+getRevDep jName aState = aState ^. revDeps ^. at jName
 
-noRevDep :: Name -> Result -> Either String Bool
-noRevDep jName = validator
-    where validator result
-            | (isLeft . fst) result = Left "Process returned error."
-            | otherwise = Right $ isLeft (getRevDep jName result)
+jobWithRevDep :: Name -> [Name] -> EitherError AppState -> EitherError Bool
+jobWithRevDep jName jRevDeps result = return . (== jRevDeps) =<< eitherGetRevDep jName =<< result
+    where eitherGetRevDep jName = maybeToEither (RevDepNotFound jName) . getRevDep jName
 
-jobInState :: Name -> JobState -> Result -> Either String Bool
-jobInState jName jState = validator
-    where validator result
-            | (isLeft . fst) result = Left "Process returned error."
-            | otherwise = fmap inState $ getJob jName result
-          inState = (== jState) . (flip (^.) jobState)
+noRevDep :: Name -> EitherError AppState -> EitherError Bool
+noRevDep jName result = return . isNothing . getRevDep jName =<< result
 
-jobWithRevDep :: Name -> [Name] -> Result -> Either String Bool
-jobWithRevDep jName jRevDeps = validator
-    where validator result
-            | (isLeft . fst) result = Left "Process returned error."
-            | otherwise = fmap (== jRevDeps) $ getRevDep jName result
-
-producedError :: JobError -> Result -> Either String Bool
-producedError jError = \m -> case fst m of
-    Left x -> Right $ x == jError
-    Right x -> Left "Job did not produce error."
-
-notProducedError :: Result -> Either String Bool
-notProducedError = Right . isRight . fst
+producedError :: JobError -> EitherError AppState -> EitherError Bool
+producedError jError (Left e) = case e of
+    EncounteredError e -> Right (e == jError)
+    e                  -> Left (UnexpectedError e)
+producedError jError (Right e) = Left (ExpectedError jError)
 
 testCreateJob :: Spec
 testCreateJob = do
     describe "createJob" $
         it "should succeed when given a unique, valid job" $
             let result = createJob' $ isolatedJob "job"
-            in (scheduler' result) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+            in scheduler' result `shouldHave` jobInState "job" (Queued LocalQueue)
 
     context "when provided with dependency in Finished state" $
         it "should be placed in the Queued state" $
@@ -98,7 +98,7 @@ testCreateJob = do
                     createJob' $ isolatedJob "dep"
                     lift $ ES.setJobState "dep" Finished
                     createJob' $ dependentJob "job" ["dep"]
-            in (scheduler' result) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+            in scheduler' result `shouldHave` jobInState "job" (Queued LocalQueue)
 
     context "when provided with dependency in an active state" $
         it "should be waiting on one job" $
@@ -106,26 +106,26 @@ testCreateJob = do
                     createJob' $ isolatedJob "dep"
                     lift $ ES.setJobState "dep" Running
                     createJob' $ dependentJob "job" ["dep"]
-            in (scheduler' result) `shouldHave` (jobInState "job" $ Waiting 1)
+            in scheduler' result `shouldHave` jobInState "job" (Waiting 1)
 
     context "when provided with a dependency" $
         it "should update dependants list for that dependency" $
             let result = do
                     createJob' $ isolatedJob "dep"
                     createJob' $ dependentJob "job" ["dep"]
-            in (scheduler' result) `shouldHave` (jobWithRevDep "dep" ["job"])
+            in scheduler' result `shouldHave` jobWithRevDep "dep" ["job"]
 
     context "when provided a job with a name that already exists" $
         it "should return JobNameUsed error" $
             let result = do
                     createJob' $ isolatedJob "job"
                     createJob' $ isolatedJob "job"
-            in (scheduler' result) `shouldHave` (producedError JobNameUsed)
+            in scheduler' result `shouldHave` producedError JobNameUsed
 
     context "when provided a dependency that doesn't exist" $
         it "should return JobMustExist error" $
             let result = createJob' $ dependentJob "job" ["dep"]
-            in (scheduler' result) `shouldHave` (producedError $ JobMustExist "dep")
+            in scheduler' result `shouldHave` producedError (JobMustExist "dep")
 
     context "when provided a dependency that has failed" $
         it "should return JobCannotHaveFailed error" $
@@ -133,7 +133,7 @@ testCreateJob = do
                     createJob' $ isolatedJob "dep"
                     lift $ ES.setJobState "dep" (Failed UserKilled)
                     createJob' $ dependentJob "job" ["dep"]
-            in (scheduler' result) `shouldHave` (producedError $ JobCannotHaveFailed "dep")
+            in scheduler' result `shouldHave` producedError (JobCannotHaveFailed "dep")
 
 testKillJob :: Spec
 testKillJob = do
@@ -142,11 +142,11 @@ testKillJob = do
             let result = do
                     createJob' $ isolatedJob "job"
                     killJob "job"
-            in (scheduler' result) `shouldHave` (jobInState "job" Killing)
+            in scheduler' result `shouldHave` jobInState "job" Killing
 
     context "when provided the name of a job that doesn't exist" $
         it "should return NoSuchJob error" $
-            (scheduler' (killJob "job")) `shouldHave` (producedError NoSuchJob)
+            scheduler' (killJob "job") `shouldHave` producedError NoSuchJob
 
     context "when provided the name of a job that is in a termination state" $
         it "should return JobMustBeTerminated error" $
@@ -154,16 +154,16 @@ testKillJob = do
                     createJob' $ isolatedJob "job"
                     lift $ ES.setJobState "job" Finished
                     killJob "job"
-            in (scheduler' result) `shouldHave` (producedError $ JobMustBeTerminated False)
+            in scheduler' result `shouldHave` producedError (JobMustBeTerminated False)
 
 testDeleteJob :: Spec
 testDeleteJob = do
     it "should remove the finished job with the given name from application state" $
         let result = do
-                createJob' $ isolatedJob "job"
-                lift $ ES.setJobState "job" Finished
-                deleteJob "job"
-        in (scheduler' result) `shouldHave` (noJob "job")
+             createJob' $ isolatedJob "job"
+             lift $ ES.setJobState "job" Finished
+             deleteJob "job"
+        in scheduler' result `shouldHave` noJob "job"
 
     context "when provided the name of a job in an active state" $
         it "should return JobMustBeTerminated error" $
@@ -171,7 +171,7 @@ testDeleteJob = do
                     createJob' $ isolatedJob "job"
                     lift $ ES.setJobState "job" Running
                     deleteJob "job"
-            in (scheduler' result) `shouldHave` (producedError $ JobMustBeTerminated True)
+            in scheduler' result `shouldHave` producedError (JobMustBeTerminated True)
 
     context "when provided the name of a job that has outstanding dependants" $
         it "should return OutstandingDependants error" $
@@ -180,13 +180,12 @@ testDeleteJob = do
                     lift $ ES.setJobState "dep" Finished
                     createJob' $ dependentJob "job" ["dep"]
                     deleteJob "dep"
-            in (scheduler' result) `shouldHave` (producedError $ OutstandingDependants ["job"])
+            in scheduler' result `shouldHave` producedError (OutstandingDependants ["job"])
 
 testUpdateJobs :: Spec
 testUpdateJobs = let
-        updated :: Scheduler -> [(Name, JobState)] -> Result
-        updated m statuses = scheduler s $ lift (updateJobs (s ^. jobs) statuses)
-            where (_, s) = scheduler' m
+        updated :: Scheduler -> [(Name, JobState)] -> EitherError AppState
+        updated m statuses = scheduler' m >>= \s -> scheduler s $ lift (updateJobs (s ^. jobs) statuses)
     in do
         describe "updateJobs" $
             it "should do nothing when job status hasn't changed" $
@@ -194,7 +193,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" (Queued LocalQueue)
                     statuses = [("job", Queued LocalQueue)]
-                in (updated result statuses) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+                in updated result statuses `shouldHave` jobInState "job" (Queued LocalQueue)
 
         context "when job is on scheduler and no new status information is received" $
             it "should do nothing" $
@@ -202,14 +201,14 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" (Queued LocalQueue)
                     statuses = [("job", Queued LocalQueue)]
-            in (updated result statuses) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+            in updated result statuses `shouldHave` jobInState "job" (Queued LocalQueue)
 
         context "when job is in state Killing and no updated information is provided" $
             it "should change job to Failed UserKilled state" $
                 let result = do
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Killing
-                in (updated result []) `shouldHave` (jobInState "job" $ Failed UserKilled)
+                in updated result [] `shouldHave` jobInState "job" (Failed UserKilled)
 
         context "when job is in state Killing and is updated to state Finished" $
             it "should change job to Failed UserKilled state" $
@@ -217,7 +216,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Killing
                     statuses = [("job", Finished)]
-                in (updated result statuses) `shouldHave` (jobInState "job" $ Failed UserKilled)
+                in updated result statuses `shouldHave` jobInState "job" (Failed UserKilled)
 
         context "when job is in state Killing and is updated to state RunError SchedulerLost" $
             it "should change job to RunError SchedulerLost state" $
@@ -225,7 +224,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Killing
                     statuses = [("job", RunError SchedulerLost)]
-                in (updated result statuses) `shouldHave` (jobInState "job" $ RunError SchedulerLost)
+                in updated result statuses `shouldHave` jobInState "job" (RunError SchedulerLost)
 
         context "when job is in state Killing and is updated to state Running" $
             it "should change job to Killing state" $
@@ -233,21 +232,21 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Killing
                     statuses = [("job", Running)]
-                in (updated result statuses) `shouldHave` (jobInState "job" $ Killing)
+                in updated result statuses `shouldHave` jobInState "job" Killing
 
         context "when job is on scheduler and is provided no state information" $
             it "should change job to RunError SchedulerLost state" $
                 let result = do
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Running
-                in (updated result []) `shouldHave` (jobInState "job" $ RunError SchedulerLost)
+                in updated result [] `shouldHave` jobInState "job" (RunError SchedulerLost)
 
         context "when job is not on scheduler and is provided no state information" $
             it "should change job to Queued LocalQueue state" $
                 let result = do
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" (Queued LocalQueue)
-                in (updated result []) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+                in updated result [] `shouldHave` jobInState "job" (Queued LocalQueue)
 
         context "when provided job state that is an expected transition from current state" $
             it "should change job to the provided state" $
@@ -255,7 +254,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" $ Queued LocalQueue
                     statuses = [("job", Running)]
-                in (updated result statuses) `shouldHave` (jobInState "job" Running)
+                in updated result statuses `shouldHave` jobInState "job" Running
 
         context "when provided job state is not an expected transition from current state" $
             it "should change job to RunError BadSchedulerTransition state" $
@@ -263,7 +262,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "job"
                         lift $ ES.setJobState "job" Running
                     statuses = [("job", Waiting 1)]
-                in (updated result statuses) `shouldHave` (jobInState "job" $ RunError BadSchedulerTransition)
+                in updated result statuses `shouldHave` jobInState "job" (RunError BadSchedulerTransition)
 
         context "when job does not transition into a termination state" $
             it "should not change the state of any dependent jobs" $
@@ -273,8 +272,8 @@ testUpdateJobs = let
                         createJob' $ dependentJob "job" ["dep"]
                     statuses = [("dep", Running)]
                 in do
-                    (updated result statuses) `shouldHave` (jobInState "dep" $ Running)
-                    (updated result statuses) `shouldHave` (jobInState "job" $ Waiting 1)
+                    updated result statuses `shouldHave` jobInState "dep" Running
+                    updated result statuses `shouldHave` jobInState "job" (Waiting 1)
 
         context "when job transitions into non-Finished termination state" $
             it "should change the state of dependent jobs to Failed DependencyFailed" $
@@ -284,8 +283,8 @@ testUpdateJobs = let
                         createJob' $ dependentJob "job" ["dep"]
                     statuses = [("dep", Failed UserKilled)]
                 in do
-                    (updated result statuses) `shouldHave` (jobInState "dep" $ Failed UserKilled)
-                    (updated result statuses) `shouldHave` (jobInState "job" $ Failed (DependencyFailed "dep"))
+                    updated result statuses `shouldHave` jobInState "dep" (Failed UserKilled)
+                    updated result statuses `shouldHave` jobInState "job" (Failed (DependencyFailed "dep"))
 
         context "when job transitions into Finished state" $
             it "should change the state of exclusively dependent job to Queued LocalQueue" $
@@ -295,8 +294,8 @@ testUpdateJobs = let
                         createJob' $ dependentJob "job" ["dep"]
                     statuses = [("dep", Finished)]
                 in do
-                    (updated result statuses) `shouldHave` (jobInState "dep" Finished)
-                    (updated result statuses) `shouldHave` (jobInState "job" $ Queued LocalQueue)
+                    updated result statuses `shouldHave` jobInState "dep" Finished
+                    updated result statuses `shouldHave` jobInState "job" (Queued LocalQueue)
 
         context "when job transitions into Finished state but dependent is also waiting on another job" $
             it "should change the state of dependent job to Waiting 1" $
@@ -306,8 +305,8 @@ testUpdateJobs = let
                         createJob' $ dependentJob "job" ["dep", "otherDep"]
                     statuses = [("dep", Finished)]
                 in do
-                    (updated result statuses) `shouldHave` (jobInState "dep" Finished)
-                    (updated result statuses) `shouldHave` (jobInState "job" $ Waiting 1)
+                    updated result statuses `shouldHave` jobInState "dep" Finished
+                    updated result statuses `shouldHave` jobInState "job" (Waiting 1)
 
         context "when dependent job finishes" $
             it "should remove reverse dependency on the job on which it depended" $
@@ -315,7 +314,7 @@ testUpdateJobs = let
                         createJob' $ isolatedJob "dep"
                         createJob' $ dependentJob "job" ["dep"]
                     statuses = [("dep", Finished), ("job", Finished)]
-                in (updated result statuses) `shouldHave` (noRevDep "dep")
+                in updated result statuses `shouldHave` noRevDep "dep"
 
 testState :: Spec
 testState = do
